28685cc5ddcb1f6042b5930528f3dd172cf4de90
İrem Koşar <99809817+iremkosar@users.noreply.github.com>
2026-02-06 00:43:14 +0300

    Migration add

--- Project2IdentityEmail/Migrations/20260205214149_migAddConfirmCode.Designer.cs
+++ Project2IdentityEmail/Migrations/20260205214149_migAddConfirmCode.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Project2IdentityEmail.Context;
 
@@ -11,9 +12,10 @@ using Project2IdentityEmail.Context;
 namespace Project2IdentityEmail.Migrations
 {
     [DbContext(typeof(EmailContext))]
-    partial class EmailContextModelSnapshot : ModelSnapshot
+    [Migration("20260205214149_migAddConfirmCode")]
+    partial class migAddConfirmCode
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Project2IdentityEmail/Migrations/20260205214149_migAddConfirmCode.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project2IdentityEmail.Migrations
+{
+    public partial class migAddConfirmCode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ConfirmCode",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConfirmCode",
+                table: "AspNetUsers");
+        }
+    }
+}
