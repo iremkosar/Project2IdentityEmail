8a25828a2baee0edd974d6918c3eddf236c33388
İrem Koşar <99809817+iremkosar@users.noreply.github.com>
2026-03-02 16:33:26 +0300

    Update MessageController1.cs

--- Project2IdentityEmail/Controllers/MessageController1.cs
+++ Project2IdentityEmail/Controllers/MessageController1.cs
@@ -97,7 +97,7 @@ namespace Project2IdentityEmail.Controllers
 
             using var client = new SmtpClient();
             client.Connect("smtp.gmail.com", 587, false);
-            client.Authenticate("iremkosar97@gmail.com", "ukky vjjj jofu hefr");
+            client.Authenticate("iremkosar97@gmail.com", "");
             client.Send(mimeMessage);
             client.Disconnect(true);
 
